f545ccc28fbe3fdaa8dc61cbc4db43e573fa5032
sreynolds2 <sara.reynolds@huskers.unl.edu>
2021-06-11 09:18:15 -0500

    extra reps and N0 update

--- R/6_initial-populations.R
+++ R/6_initial-populations.R
@@ -1,30 +1,148 @@
 
 library(plyr)
+library(data.table)
+#library(tabulizer) -- NOT WORKING
 
+
+source("./R/0_default-parameters.r")
 # GENERATE INITIAL POPULATION
 init_pop<- function(inputs=NULL,
-                    type=NULL) # "Uniform" OR "2020_est"
+                    type=NULL) # "Uniform", "2020_Rotella" -- NOT READY 
+                               # OR "2020_PSPAP"
 {
   inps<- inputs
+  ## 2020 AMCR MEDIAN POP EST
+  inps$N_H<- 11853
+  inps$N_W<- 505
+  inps$sexratio_H<- 0.5
+  inps$sexratio_W<- 0.32
+  # SOURCE: THE 40:85 ADULT F:M RATIO ESTIMATED BY JAEGER ET AL. (2009) 
+  # IN THE 2017 AM REPORT (TABLE 3-1)
+  inps$N0_type<- type
   if(type=="Uniform")
   {
-    ## 2020 AMCR MEDIAN POP EST
-    inps$N_H<- 11853
-    inps$N_W<- 505
-    inps$sexratio_H<- 0.5
-    inps$sexratio_W<- 0.32
-      # SOURCE: THE 40:85 ADULT F:M RATIO ESTIMATED BY JAEGER ET AL. (2009) 
-      # IN THE 2017 AM REPORT (TABLE 3-1) 
     N<- ceiling(inps$N_H*inps$sexratio_H+inps$N_W*inps$sexratio_W)
     N0<- rep(floor(N/inps$max_age), inps$max_age)
     N0<- N0+c(rep(1, N-sum(N0)), rep(0, inps$max_age-N+sum(N0)))
     inps$N0<- N0
-    inps$N0_type<- type
   }
-  if(type=="2020_est")
+  if(type=="2020_PSPAP")
   {
+    f_dat<- fread("../PSPAP-data/fish-processed.csv")
+    names(f_dat)<-tolower(names(f_dat))
+    f_dat[,rpma:=4]
+    # set rpma for segments 5 and 6
+    setDT(f_dat)[segment_id%in%c(5,6,23),rpma:=3]
+    # set rpma for segment 1-4
+    setDT(f_dat)[segment_id%in%c(1,2,3,4,21,22,51,52,53,54,55),rpma:=2]# 21 is Milk, 22 is YSR
+    # SUBSET OUT UPPER RIVER FISH IN MOST RECENT YEARS
+    f_dat<- subset(f_dat, rpma==2 & year>=2018)
+    # READ IN STOCKING DATA
+    stocked<- fread("../PSPAP-data/stocking-processed.csv")
+    # REMOVE STOCKINGS WITH MISSING SPAWN DATE
+    stocked[,spawn_date:= spawn.date]
+    stocked<- subset(stocked, !is.na(spawn_date))
+    # PULL RELEVANT COLUMNTS
+    indx<- match(c("tag_number","spawn_date"), names(stocked))
+    spawn<- stocked[,..indx]
+    # REMOVE STOCKINGS WITH MISSING TAG NUMBERS
+    tags<- unique(spawn$tag_number)
+    tags<- tags[order(tags)]
+    setDT(spawn)[tag_number %in% c("",tags[length(tags)], "..........","...",".",
+                                   "0000000000","N/A","NOFISHSCAN","unknown",
+                                   "XXXXXXXXXX","CAUDAL CL","N PIT"),
+                 tag_number:="-99"]
+    setDT(spawn)[is.na(tag_number),tag_number:="-99"]    
+    setDT(spawn)[tag_number=="NA",tag_number:="-99"] 
+    spawn<- subset(spawn, tag_number!="-99")
+    # REMOVE DUPLICATE DATA
+    spawn<- spawn[!duplicated(spawn),]
+    # ADD SPAWNING DATE TO FISH DATA
+    f_dat<- merge(f_dat, spawn, all.x=TRUE)
+    # SUBSET OUT UNKNOWN AGE FISH
+    f_dat<- subset(f_dat, !is.na(spawn_date))
+    # KEEP MOST RECENT CAPTURE OF KNOWN AGE FISH
+    tags<- f_dat[which(duplicated(f_dat$tag_number)),]$tag_number
+    f_ids<- f_dat[which(f_dat$tag_number %in% tags),]$f_id
+    indx<- sapply(tags, function(x)
+    {
+      tmp<- f_dat[which(f_dat$tag_number==x),]
+      return(tmp[which.max(tmp$setdate),]$f_id)
+      
+    })
+    f_ids<- setdiff(f_ids, indx)
+    f_dat<- f_dat[-which(f_dat$f_id %in% f_ids),]
+    rm(f_ids, tags, indx)
+    # ADD IN AGE AT CAPTURE
+    f_dat$age_capture<- floor(as.numeric(difftime(f_dat$setdate, 
+                                                  f_dat$spawn_date,
+                                                  units="days")/365.25))
+    #f_dat$age<- as.numeric(difftime(f_dat$setdate, 
+    #                                    dat$spawn.date,
+    #                                    units="days")/365.25)
+    ## CORRECT TYPO (BY INSPECTION OF DATA) IN SPAWN DATE 
+    #dat[which(dat$stock_age<0),]$spawn.date<- as.POSIXct("1992-06-21")
     
+    # COMPUTE AGE AT END OF 2020
+    f_dat$age_2020<- as.numeric(difftime(as.POSIXct("2020-12-31"), 
+                                         f_dat$spawn_date,
+                                         units="days")/365.25)
+    # AGGREGATE TO PROPORTIONS
+    f_dat$age<- floor(f_dat$age_2020)
+    f_dat$yr_diff<- f_dat$age-f_dat$age_capture
+    f_dat$freq<- sapply(1:nrow(f_dat), function(i)
+    {
+      ifelse(f_dat$yr_diff[i]==0, 1,
+             prod(inps$phi[f_dat$age_capture[i]:
+                             (f_dat$age_capture[i] + f_dat$yr_diff[i]-1)]))
+    })
+    est<- aggregate(freq~age, f_dat, sum)
+    est$prop<- est$freq/sum(est$freq)
+    est$N0<- round(inps$N_H*inps$sexratio_H*est$prop)
+    est<- merge(est, data.frame(age=1:100), all=TRUE)
+    est<- est[order(est$age),]
+    # NO STOCKING IN 2020; ALL 2018 STOCKING SPAWNED IN 2017
+    # ALL 2019 STOCKINGS FROM 2019 SPAWNING WERE DRIFT STUDY FISH
+    est$N0[1]<- round(sum(stocked[which(stocked$year.stocked==2019 &
+                                          as.numeric(format(stocked$spawn_date, "%Y"))==2019),]$numbers.stocked)*inps$phi0_MR*1)
+    est$N0[2]<- round(sum(stocked[which(stocked$year.stocked==2019 &
+                                          as.numeric(format(stocked$spawn_date, "%Y"))==2018),]$numbers.stocked)*prod(inps$phi[1]))
+    est[is.na(est)]<- 0
+    est[which(est$age %in% 68:100),]$N0<- rmultinom(1, ceiling(inps$N_W*inps$sexratio_W), rep(1/33, 33))
+    inps$N0<- est$N0
   }
+  # if(type=="2020_Rotella")
+  # {
+  #   # READ IN ROTELLA 2016 ESTIMATES BY AGE
+  #   s_dat<- extract_tables("./baseline-parameters/Rotella_2017_Update.pdf", 
+  #                          pages=c(92))
+  #   MO_Fing_Monthly<- as.data.frame(MO_Fing_Monthly[[1]][4:nrow(MO_Fing_Monthly[[1]]),])
+  #   
+  #   # READ IN STOCKING DATA
+  #   stocked<- fread("../PSPAP-data/stocking-processed.csv")
+  #   # SUBSET TO POST-2016
+  #   stocked<- subset(stocked, year.stocked>=2017)
+  #   # REMOVE STOCKINGS WITH MISSING SPAWN DATE
+  #   stocked[,spawn_date:= spawn.date]
+  #   stocked<- subset(stocked, !is.na(spawn_date))
+  #   # ADD IN STOCKING AGE
+  #   stocked$age_stocked<- round(as.numeric(difftime(stocked$stock.date, 
+  #                                                   stocked$spawn_date,
+  #                                                   units="days")/365.25))
+  #   # ADD IN 2020 AGE
+  #   stocked$age_2020<- round(as.numeric(difftime(as.POSIXct("2020-09-30"), 
+  #                                                stocked$spawn_date,
+  #                                                units="days")/365.25))
+  #   stocked$yr_diff<- stocked$age_2020-stocked$age_stocked
+  #   phi<- c(inps$phi0_MR, inps$phi)
+  #   stocked$N_2020<- sapply(1:nrow(stocked), function(i)
+  #   {
+  #     stocked$numbers.stocked[i]*
+  #       prod(phi[(stocked$age_stocked[i]+1):(stocked$age_stocked[i]+stocked$yr_diff[i])])
+  #   })
+  #   est<- aggregate(N_2020~age_2020, stocked, sum)
+  #   est$N0<- round(est$N_2020)
+  # }
   return(inps)
 }
 
@@ -174,20 +292,20 @@ pseudo_extinct<- function(pop_data=NULL,
   # COMPUTE FRACTION BELOW THRESHOLD FOR EACH ALTERNATIVE
   out<- lapply(1:7, function(x)
   {
-      tmp<- pop_data[[x]]
-      tmp<- tmp[,1:reps]
-      yr<- sapply(1:reps, function(z)
-      {
-        val<- ifelse(all(tmp[,z]>=threshold),
-                     years*10,
-                     min(which(tmp[,z]<threshold)))
-        return(val)
-      })
-      frac<- sapply(2:(years+1), function(y)
-      {
-        val<- length(which(yr<=y))/reps
-      })
-      return(list(extinction_yr=yr, frac_extinct=frac))
+    tmp<- pop_data[[x]]
+    tmp<- tmp[,1:reps]
+    yr<- sapply(1:reps, function(z)
+    {
+      val<- ifelse(all(tmp[,z]>=threshold),
+                   years*10,
+                   min(which(tmp[,z]<threshold)))
+      return(val)
+    })
+    frac<- sapply(2:(years+1), function(y)
+    {
+      val<- length(which(yr<=y))/reps
+    })
+    return(list(extinction_yr=yr, frac_extinct=frac))
   })
   names(out)<-names(pop_data)[1:7]
   pop_data$extinction<-out
@@ -226,10 +344,61 @@ frac_extinct<- function(ext_data=NULL,
   return(out)
 }
 
+time_extinct<- function(ext_data=NULL,
+                        threshold=50,
+                        years=200,
+                        reps=5000)
+{
+  # ERROR CHECK
+  if(threshold!=ext_data$extinction$threshold)
+  {
+    return(print("Extinction data is not for the given threshold."))
+  }
+  if(years>length(ext_data$extinction[[1]]$frac_extinct))
+  {
+    return(print("Extinction data does not contain enough years for the desired estimate."))
+  }
+  if(reps>length(ext_data$extinction[[1]]$extinction_yr))
+  {
+    return(print("Extinction data does not contain enough replicates for the desired estimate."))
+  }
+  # PULL DESIRED ESTIMATE FOR EACH ALTERNATIVE
+  alt_out<- lapply(1:7, function(x)
+  {
+    dat<- ext_data$extinction[[x]]
+    if(all(dat$extinction_yr[1:reps]<=years))
+    {
+      tmp<- data.frame(extinction_yr=dat$extinction_yr[1:reps], freq=1)
+      tmp<- aggregate(freq~extinction_yr, tmp, sum)
+      tmp$prob<- tmp$freq/sum(tmp$freq)
+      exp_time<- sum(tmp$extinction_yr*tmp$prob)
+    }
+    if(!all(dat$extinction_yr[1:reps]<=years))
+    {
+      exp_time<- -99
+    }
+    if(reps==length(dat$extinction_yr))
+    {
+      time_eighty<- min(which(dat$frac_extinct>=0.8))
+    }
+    if(reps<length(dat$extinction_yr))
+    {
+      frac_extinct<- dat$frac_extinct*length(dat$extinction_yr)/reps
+      time_eighty<- min(which(frac_extinct>=0.8))
+    }
+    out<- data.frame(flow_scenario=names(ext_data$extinction)[x],
+                     E_time=exp_time,
+                     time_80=time_eighty)
+    return(out)
+  })
+  alt_out<- do.call("rbind", alt_out)
+  alt_out$param_id<- ext_data$inputs$param_id
+  return(alt_out)
+}
 
 # OBTAIN INPUT DATA
 source("./R/0_default-parameters.r")
-                           
+
 # OBTAIN TEMPERATURE, RETENTION, AND SPAWNING DATA
 ## PULL ANNUAL TEMPERATURE DATA
 temps<- read.csv("./dat/Observed_Annual_Temperature_Class.csv")
@@ -270,12 +439,14 @@ trans<- read.csv("./output/_stochastic/Temperature_Transition_Matrix.csv",
 temp_freq<- read.csv("./output/_stochastic/Temperature_Frequency.csv")
 
 
-# # SIMULATE ENVIRONMENT
-# inps<- project_environ(inputs = inputs,
-#                        temp_data = temps,
-#                        transitions = trans,
-#                        temp_freq_data = temp_freq,
-#                        id=1)
+# SIMULATE ENVIRONMENT
+inps<- project_environ(inputs = inputs,
+                       temp_data = temps,
+                       transitions = trans,
+                       temp_freq_data = temp_freq,
+                       reps = 3000,
+                       id=2)
+
 getinps<- readRDS("./output/_stochastic/Ntot_1-1.rds")
 inps<- inputs
 inps$environment<- getinps$inputs$environment
@@ -538,36 +709,90 @@ rm(getinps)
 
 # SAVE PARAMETER VALUES FOR SENSITIVITY-ELASTICITY ANALYSES
 # params<- params[order(params$param_id), c(2,1,3:ncol(params))]
-# write.csv(params, "./output/_stochastic/sens_elas_vals.csv")
+# write.csv(params, "./output/_stochastic/sens_elas_vals.csv",
+#           row.names = FALSE)
 params<- read.csv("./output/_stochastic/sens_elas_vals.csv")
 
 ## EXTINCTION ANALYSES
-library(parallel)
-numCores<- detectCores()
-cl<- makeCluster(numCores)
-clusterExport(cl, c("pseudo_extinct"))
-ptm<-proc.time()
-invisible(parLapply(cl, 388:561, function(p)
-{
-  dat<- readRDS(paste0("./output/_stochastic/Ntot_1-", p, ".rds"))
-  out<-pseudo_extinct(pop_data = dat, threshold = 50,
-                      years=200, reps=5000)
-  saveRDS(out, paste0("./output/_stochastic/Full_Data_1-", p, ".rds"))
-}))
-tot<-(proc.time()-ptm)[3]/60
-tot
-stopCluster(cl)
-rm(cl)
+# library(parallel)
+# numCores<- detectCores()
+# cl<- makeCluster(numCores)
+# clusterExport(cl, c("pseudo_extinct"))
+# ptm<-proc.time()
+# invisible(parLapply(cl, 1:561, function(p)
+# {
+#   dat<- readRDS(paste0("./output/_stochastic/Ntot_1-", p, ".rds"))
+#   out<-pseudo_extinct(pop_data = dat, threshold = 50,
+#                       years=200, reps=5000)
+#   saveRDS(out, paste0("./output/_stochastic/Full_Data_1-", p, ".rds"))
+# }))
+# tot<-(proc.time()-ptm)[3]/60
+# tot
+# stopCluster(cl)
+# rm(cl)
+# 
+# library(parallel)
+# numCores<- detectCores()
+# cl<- makeCluster(numCores)
+# clusterExport(cl, c("frac_extinct"))
+# ptm<-proc.time()
+# explore_ext<- parLapply(cl, 1:567, function(p)
+# {
+#   dat<- readRDS(paste0("./output/_stochastic/Full_Data_1-", p, ".rds"))
+#   yr_test<- lapply(seq(75, 200, 25), function(y)
+#   {
+#     rep_no_test<- lapply(seq(1000, 5000, 1000), function(r)
+#     {
+#       out<- frac_extinct(ext_data = dat, threshold = 50,
+#                          years=y, reps=r)
+#       out$reps<- r
+#       return(out)
+#     })
+#     rep_no_test<- do.call("rbind", rep_no_test)
+#     rep_no_test$years<- y
+#     return(rep_no_test)
+#   })
+#   yr_test<- do.call("rbind", yr_test)
+#   return(yr_test)
+# })
+# ext_test<- do.call("rbind", explore_ext)
+# tot<-(proc.time()-ptm)[3]/60
+# tot
+# stopCluster(cl)
+# rm(cl)
+# ext_test<- ext_test[order(ext_test$param_id,
+#                           ext_test$years,
+#                           ext_test$reps),]
+# write.csv(ext_test, "./output/_stochastic/extinction_results.csv",
+#           row.names = FALSE)
 
+## COMPARE YEAR AND REP DATA
+ext<- read.csv("./output/_stochastic/extinction_results.csv",
+               stringsAsFactors = FALSE)
+bline<- ext[ext$param_id==1,]
+alts<- unique(bline$flow_scenario)
+yrs<- unique(bline$years)
+### YEAR TEST
+#### ALL RUNS EXTINCT BY 100 YEARS FOR BASELINE AGE-0 SURVIVAL 
+tmp<- ext[ext$years>=100 & !(ext$param_id%in%2:13),]
+unique(tmp$frac_extinct)
+#### AFTER 75 YEARS VALUES HAVEN'T CONVERGED WITH 5000 REPS
+frac_diff<-sapply(alts, function(x)
+{
+  tmp<- bline[bline$years==75 & bline$flow_scenario==x,]
+  out<- min(abs(diff(tmp$frac_extinct)))
+  return(out)
+})
+max(frac_diff)
+#### RUN FINER YEAR MESH
 library(parallel)
 numCores<- detectCores()
 cl<- makeCluster(numCores)
 clusterExport(cl, c("frac_extinct"))
-ptm<-proc.time()
 explore_ext<- parLapply(cl, 1:567, function(p)
 {
   dat<- readRDS(paste0("./output/_stochastic/Full_Data_1-", p, ".rds"))
-  yr_test<- lapply(seq(75, 200, 25), function(y)
+  yr_test<- lapply(seq(80, 95, 5), function(y)
   {
     rep_no_test<- lapply(seq(1000, 5000, 1000), function(r)
     {
@@ -583,17 +808,96 @@ explore_ext<- parLapply(cl, 1:567, function(p)
   yr_test<- do.call("rbind", yr_test)
   return(yr_test)
 })
-ext_test<- do.call("rbind", explore_ext)
-tot<-(proc.time()-ptm)[3]/60
-tot
+explore_ext<- do.call("rbind", explore_ext)
 stopCluster(cl)
 rm(cl)
-ext_test<- ext_test[order(ext_test$param_id,
-                          ext_test$years,
-                          ext_test$reps),]
-write.csv(ext_test, "./output/_stochastic/extinction_results.csv",
+ext<- rbind(ext, explore_ext)
+ext<- ext[order(ext$param_id,
+                ext$years,
+                ext$reps),]
+write.csv(ext, "./output/_stochastic/extinction_results.csv",
           row.names = FALSE)
 
+#### RETEST
+bline<- ext[ext$param_id==1,]
+alts<- unique(bline$flow_scenario)
+yrs<- unique(bline$years)
+
+frac_diff<-sapply(seq(75, 95, 5), function(y)
+{
+  outt<- sapply(alts, function(x)
+  {
+    tmp<- bline[bline$years==y & bline$flow_scenario==x,]
+    out<- min(abs(diff(tmp$frac_extinct)))
+    return(out)
+  })
+  return(max(outt))
+})
+frac_diff
+
+frac_range<-sapply(seq(75, 95, 5), function(y)
+{
+  tmp<- bline[bline$years==y & bline$reps==5000,]
+  out<- max(tmp$frac_extinct)-min(tmp$frac_extinct)
+  return(out)
+})
+frac_range
+
+## EXTRA SIMULATIONS
+dat1<- readRDS("./output/_stochastic/Ntot_1-1.rds")
+dat2<- readRDS("./output/_stochastic/Ntot_2-1.rds")
+dat1$`1`<- cbind(dat1$`1`, dat2$`1`)
+dat1$`1a`<- cbind(dat1$`1a`, dat2$`1a`)
+dat1$`1b`<- cbind(dat1$`1b`, dat2$`1b`)
+dat1$`2`<- cbind(dat1$`2`, dat2$`2`)
+dat1$`2a`<- cbind(dat1$`2a`, dat2$`2a`)
+dat1$`2b`<- cbind(dat1$`2b`, dat2$`2b`)
+dat1$NoAct<- cbind(dat1$NoAct, dat2$NoAct)
+dat1$inputs$environment$environ_paths<- 
+  cbind(dat1$inputs$environment$environ_paths, dat2$inputs$environment$environ_paths)
+dat1$inputs$environment$notes<- "Environment paths 1 and 2 combined."
+dat1$inputs$reps<- dat1$inputs$reps + dat2$inputs$reps
+saveRDS(dat1, "./output/_stochastic/Ntot_0-1.rds")
+rm(dat2)
+out<-pseudo_extinct(pop_data = dat1, threshold = 50,
+                    years=200, reps=8000)
+saveRDS(out, "./output/_stochastic/Full_Data_0-1.rds")
+
+dat<- out
+rm(dat1, out)
+yr_test<- lapply(c(seq(75, 100, 5), seq(125, 200, 25)), function(y)
+{
+  rep_no_test<- lapply(seq(1000, 8000, 1000), function(r)
+  {
+    out<- frac_extinct(ext_data = dat, threshold = 50,
+                       years=y, reps=r)
+    out$reps<- r
+    return(out)
+  })
+  rep_no_test<- do.call("rbind", rep_no_test)
+  rep_no_test$years<- y
+  return(rep_no_test)
+})
+yr_test<- do.call("rbind", yr_test)
+ext<- read.csv("./output/_stochastic/extinction_results.csv",
+               stringsAsFactors = FALSE)
+ext<- ext[which(ext$param_id!=1),]
+ext<- rbind(ext, yr_test)
+ext<- ext[order(ext$param_id,
+                ext$years,
+                ext$reps),]
+write.csv(ext, "./output/_stochastic/extinction_results.csv",
+          row.names = FALSE)
+
+bline<- ext[ext$param_id==1,]
+alts<- unique(bline$flow_scenario)
+yrs<- unique(bline$years)
+
+tmp<- bline[bline$years==yrs[1] & bline$flow_scenario==alts[1],]
+plot(tmp$reps, tmp$frac_extinct)
+
+
+## COMPUTE SENSITIVITIES AND ELASTICITIES
 ## SPAWNING ANALYSIS
 
 
@@ -639,9 +943,3 @@ write.csv(ext_test, "./output/_stochastic/extinction_results.csv",
 # names(out)<- alts
 # out$inputs<- inps
 # saveRDS(out, "./output/_stochastic/Loglt_Test.rds")
-
-
-
-
-
-
